5efed2748ae8365d57c411f1673e36f213c03d63
woywro <woywro@gmail.com>
2024-07-05 12:44:12 +0200

    cc

--- example/src/components/Component3/index.tsx
+++ example/src/components/Component3/index.tsx
@@ -9,7 +9,7 @@ const Component3 = () => {
   useEffect(() => {
     console.log('Starting heavy computation...')
     let largeArray: number[] = []
-    for (let i = 0; i < 1e5; i++) {
+    for (let i = 0; i < 500000; i++) {
       largeArray.push(Math.random())
     }
     largeArray.sort((a, b) => a - b)
